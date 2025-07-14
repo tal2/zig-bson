38d87da22ffbc102f4c4ff3440af3327270ca3f0
tal2 <tal.zajicek@gmail.com>
2025-07-14 18:22:40 +0300

    add support for reading BsonDocument from reader

--- src/bson.zig
+++ src/bson.zig
@@ -8,6 +8,34 @@ pub const BsonDocument = struct {
     len: usize,
     raw_data: []const u8,
 
+    pub fn readDocument(allocator: Allocator, reader: anytype) !*BsonDocument {
+        const document_len = try reader.readInt(i32, .little);
+        const size = @as(usize, @intCast(document_len));
+
+        var raw_data = try allocator.alloc(u8, size);
+        errdefer allocator.free(raw_data);
+
+        @memcpy(raw_data[0..@sizeOf(i32)], std.mem.asBytes(&document_len));
+
+        var pos: usize = @sizeOf(i32);
+
+        while (pos < size) {
+            const amt = try reader.read(raw_data[pos..]);
+            if (amt == 0) {
+                return error.EndOfStream;
+            }
+            pos += amt;
+        }
+
+        std.debug.assert(pos == size);
+
+        var document = try allocator.create(BsonDocument);
+        errdefer document.deinit(allocator);
+        document.len = size;
+        document.raw_data = raw_data;
+        return document;
+    }
+
     pub fn deinit(self: *BsonDocument, allocator: Allocator) void {
         allocator.free(self.raw_data);
         allocator.destroy(self);

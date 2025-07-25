06b865e1a0cd6097024043376829b10ef4de48b2
tal2 <tal.zajicek@gmail.com>
2025-07-25 16:40:01 +0300

    update build.zig - remove exe related code

--- build.zig
+++ build.zig
@@ -17,14 +17,6 @@ pub fn build(b: *std.Build) void {
     const zig_decimal_mod = zig_decimal_dep.module("zig_decimal_module");
     lib_mod.addImport("binary_coded_decimal", zig_decimal_mod);
 
-    const exe_mod = b.createModule(.{
-        .root_source_file = b.path("src/main.zig"),
-        .target = target,
-        .optimize = optimize,
-    });
-
-    exe_mod.addImport("binary_coded_decimal", zig_decimal_mod);
-
     const lib = b.addLibrary(.{
         .linkage = .static,
         .name = "bson",
@@ -33,37 +25,12 @@ pub fn build(b: *std.Build) void {
 
     b.installArtifact(lib);
 
-    const exe = b.addExecutable(.{
-        .name = "bson",
-        .root_module = exe_mod,
-    });
-
-    b.installArtifact(exe);
-
-    const run_cmd = b.addRunArtifact(exe);
-
-    run_cmd.step.dependOn(b.getInstallStep());
-
-    if (b.args) |args| {
-        run_cmd.addArgs(args);
-    }
-
-    const run_step = b.step("run", "Run the app");
-    run_step.dependOn(&run_cmd.step);
-
     const lib_unit_tests = b.addTest(.{
         .root_module = lib_mod,
     });
 
     const run_lib_unit_tests = b.addRunArtifact(lib_unit_tests);
 
-    // const exe_unit_tests = b.addTest(.{
-    //     .root_module = exe_mod,
-    // });
-
-    // const run_exe_unit_tests = b.addRunArtifact(exe_unit_tests);
-
     const test_step = b.step("test", "Run unit tests");
     test_step.dependOn(&run_lib_unit_tests.step);
-    // test_step.dependOn(&run_exe_unit_tests.step);
 }
--- src/main.zig
+++
@@ -1,4 +0,0 @@
-const std = @import("std");
-const testing = std.testing;
-
-pub fn main() !void {}
